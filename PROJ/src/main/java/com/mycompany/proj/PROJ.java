777d4b9c97b15e3535ce08a607db1e587529b295
tep <sephrefil011@gmail.com>
2024-10-15 10:24:53 +0800

    Add files via upload

---
+++ PROJ/src/main/java/com/mycompany/proj/PROJ.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proj;
+
+/**
+ *
+ * @author CL3-03
+ */
+public class PROJ {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
